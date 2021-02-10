bad50d9cfb558c196c12466f5a6f234a0af9086d
NWT27 <jalloyd@ad.unc.edu>
2021-02-09 21:26:22 -0500

    final

---
+++ exercises/ex02_p1/fortune_cookie.py
@@ -0,0 +1,33 @@
+"""Fortune cookie exercise redux as a structured program."""
+
+from random import randint
+
+__author__ = "730400848"
+
+
+def main() -> None:
+    """The entrypoint of the program, when run as a module."""
+    print("Your fortune cookie says...")
+    print(fortune_cookie())
+    print("Now, go spread positive vibes!")
+
+
+def fortune_cookie() -> int:
+    
+    fortune: int = randint(0, 40)
+
+    if fortune <= 20:
+        if fortune <= 10:
+            return("You a real one.")
+        else:
+            return("Somebody new will enjoy your presence today.")
+    else:
+        if fortune <= 30:
+            return("Your current path will lead to personal glory.")
+        else:
+            return("Some table is about to turn.")
+
+# Python Idiom for "starting" the program when run as a module.
+# The special dunder variable __name__ will be "__main__" when run as module. 
+if __name__ == "__main__":
+    main()
