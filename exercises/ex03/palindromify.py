2afb3489f8f75707f493c857aa7fe5c791054470
NWT27 <jalloyd@ad.unc.edu>
2021-03-05 13:57:36 -0500

    with space check

---
+++ exercises/ex03/palindromify.py
@@ -0,0 +1,53 @@
+"""EX03 - palindromify function."""
+
+__author__: str = "730400848"
+
+
+def main() -> None:
+    """Entrypoint of the program."""
+    # Put print statements here to test your function
+    print(palindromify("han", True))
+    print(palindromify("red", True))
+    print(palindromify("live on time ", False))
+    # ex. print(palindromify("race", false))
+    print(palindromify("race", False))
+
+
+def palindromify(word: str, even_odd: bool) -> str:
+    """A function to make any string a palindrome."""
+    i = 0
+    word_list: list[str] = []
+    join_add: str = ""
+    while i < len(word):
+        word_list.append(word[i])
+        i += 1
+    if even_odd == True:
+        rev_even_list: list = reversed(word_list)
+        rev_even_new: str = join_add.join(rev_even_list)
+        i = 0
+        while i < len(word_list):
+            if word_list[i] == " ":
+                palindrome_even: str = (f"{word} {rev_even_new}")
+                return palindrome_even
+            i += 1
+        palindrome_even: str = (f"{word}{rev_even_new}")
+        return palindrome_even
+
+
+    if even_odd == False:
+        i = len(word_list) - 1
+        word_list.pop(i)
+        rev_odd_list: list = reversed(word_list)
+        rev_odd_new: str = join_add.join(rev_odd_list)
+        i = 0
+        while i < len(word_list):
+            if word_list[i] == " ":
+                palindrome_odd: str = (f"{word} {rev_odd_new}")
+                return palindrome_odd
+            i += 1
+        palindrome_odd: str = (f"{word}{rev_odd_new}")
+        return palindrome_odd
+
+
+if __name__ == "__main__":
+    main()
